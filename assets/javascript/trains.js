d63b3db312b9c49846a31d1b652f8c236923e39e
jake hanes <jhanesx@yahoo.com>
2019-07-23 13:31:07 -0700

    got moment math to work and correct values on input vars

--- assets/javascript/trains.js
+++ assets/javascript/trains.js
@@ -20,43 +20,70 @@ $(document).ready(function(){
 
 const dataRef = firebase.database();
 
-let trainName = $("#tName").val().trim();
-let trainDest = $("#tDest").val().trim();
-let trainTime = $("#tTime").val().trim();
-let trainFrequency = $("#tFrequency").val().trim();
+let trainName = "";
+let trainDest = "";
+let trainTime = "";
+let trainFrequency = 0;
 
-// let currentTime = moment();
-// console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-// let diffTime = moment().diff(moment(trainTime), "minutes");
-// console.log("DIFFERENCE IN TIME: " + diffTime);
-// let tRemainder = diffTime % trainFrequency;
-// console.log(tRemainder);
-// let tMinutesTillTrain = trainFrequency - tRemainder;
-// console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+ trainName = $("#tName").val();
+ trainDest = $("#tDest").val();
+ trainTime = $("#tTime").val();
+ trainFrequency = parseInt($("#tFrequency").val());
 
 
-
-
-
-$("#addBtn").on("click", function(){
-    console.log("iv been clicked")
-    
-    let currentTime = moment();
-    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-    let diffTime = moment().diff(moment(trainTime), "minutes");
+ // let currentTime = moment();
+ // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+ // let diffTime = moment().diff(moment(trainTime), "minutes");
+ // console.log("DIFFERENCE IN TIME: " + diffTime);
+ // let tRemainder = diffTime % trainFrequency;
+ // console.log(tRemainder);
+ // let tMinutesTillTrain = trainFrequency - tRemainder;
+ // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+ 
+ 
+ 
+ 
+ 
+ $("#addBtn").on("click", function(event){
+     console.log("iv been clicked")
+     event.preventDefault();
+     trainName = $("#tName").val();
+     trainDest = $("#tDest").val();
+     trainTime = $("#tTime").val();
+     trainFrequency = parseInt($("#tFrequency").val());
+     console.log(trainFrequency);
+     
+     let firstTimeConverted = moment(trainTime, "HH:mm").subtract(1, "years");
+     console.log(firstTimeConverted);
+     
+     // Current Time
+     let currentTime = moment();
+     console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+     
+     // Difference between the times
+     let diffTime = moment().diff(moment(firstTimeConverted), "minutes");
     console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
     let tRemainder = diffTime % trainFrequency;
     console.log(tRemainder);
+
+    // Minute Until Train
     let tMinutesTillTrain = trainFrequency - tRemainder;
     console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
+    // Next Train
+    let nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
+    
 
     dataRef.ref().push({
         trainName: trainName,
         trainDest: trainDest,
-        trainTime: trainTime,
         trainFrequency: trainFrequency,
+        nextTrain: nextTrain,
+        minsAway: tMinutesTillTrain
         
     })
 })  
